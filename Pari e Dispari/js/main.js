e977afd5dcb266436c184c937aa1a46cf653359a
VincenzoTavassi <info@egadivacanze.it>
2023-01-31 19:48:36 +0100

    funzione pari dispari

--- Pari e Dispari/js/main.js
+++ Pari e Dispari/js/main.js
@@ -4,43 +4,42 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari(usando una funzione)
 // Dichiariamo chi ha vinto.
 
-// const numeroUtente = parseInt(prompt('Scegli un numero'));
 const numeroPc = numeroRandom(1, 5);
-console.log(numeroPc);
 let pariDispari = prompt('Scegli se il numero sarà pari o dispari');
-console.log(pariDispari);
 
+// controllo scrittura pari e dispari 
 while (pariDispari != 'pari' && pariDispari != 'dispari') {
     pariDispari = prompt('Scrivi solo "pari" o "dispari"');
 }
 
 let numeroUtente = parseInt(prompt('Scegli un numero da 1 a 5'));
 
+// controllo numero utente nel range e valido 
 while (isNaN(numeroUtente) || numeroUtente > 5 || numeroUtente <= 0) {
     numeroUtente = parseInt(prompt('Hai sbagliato. Scegli un numero da 1 a 5'));
 }
 
+// somma numero utente e numero pc
 let sommaNumeri = numeroPc + numeroUtente;
-console.log(sommaNumeri);
 
-let numeroPari = false;
-
-if (sommaNumeri % 2 == 0) {
-    numeroPari = true;
-}
-console.log(numeroPari);
-
-
-if (pariDispari == 'pari' && numeroPari == true) {
-    alert('Il numero era pari, hai vinto. Il numero era ' + sommaNumeri);
-} else if (pariDispari == 'dispari' && numeroPari == false) {
-    alert('Il numero era dispari, hai vinto. Il numero era ' + sommaNumeri);
+// se ciò che ritorna la funzione pari/dispari è uguale alla scelta dell'utente, ha vinto 
+if (isEven(sommaNumeri) == pariDispari) {
+    alert('Hai vinto. Il numero era ' + sommaNumeri);
 } else {
-    alert('Non hai vinto. Il numero era ' + sommaNumeri);
+    alert('Hai perso. Il numero era ' + sommaNumeri);
 }
 
 
+// funzione per stabilire se un numero è pari o dispari 
+function isEven(num) {
+    if (num % 2 == 0) {
+        return 'pari';
+    } else {
+        return 'dispari';
+    }
+}
 
+// funzione per generare  un numero random nel range indicato 
 function numeroRandom(min, max) {
     max = parseInt(max);
     min = parseInt(min);
